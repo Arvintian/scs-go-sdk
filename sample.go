ee1abf6a554923aed75dc61c8840a9bdbed00bac
Arvin <arvintian8@gmail.com>
2021-01-23 19:59:41 +0800

    debug range sample

--- sample.go
+++ sample.go
@@ -76,30 +76,67 @@ func main() {
 	// // fmt.Println("test delete bucket===========")
 	// // fmt.Println(s.DeleteBucket("test.create"))
 
-	fmt.Println("test MultipartUpload======")
-	mp, err := b.InitiateMultipartUpload("testmukey", map[string]string{
-		"x-amx-meta-mu": "foo",
-	})
-	if err == nil {
-		mps := make([]scs.Part, 0)
-		for i := 1; i <= 3; i++ {
-			p, err := b.UploadPart("testmukey", mp.UploadID, i, bytes.NewBufferString("ammmmm"))
-			if err != nil {
-				fmt.Println(err)
+	// fmt.Println("test MultipartUpload======")
+	// mp, err := b.InitiateMultipartUpload("testmukey", map[string]string{
+	// 	"x-amx-meta-mu": "foo",
+	// })
+	// if err == nil {
+	// 	mps := make([]scs.Part, 0)
+	// 	for i := 1; i <= 3; i++ {
+	// 		p, err := b.UploadPart("testmukey", mp.UploadID, i, bytes.NewBufferString("ammmmm"))
+	// 		if err != nil {
+	// 			fmt.Println(err)
+	// 		}
+	// 		mps = append(mps, p)
+	// 	}
+	// 	lps, err := b.ListParts(mp.Key, mp.UploadID)
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
+	// 	fmt.Println(mps)
+	// 	fmt.Println(lps)
+	// 	b.CompleteMultipartUpload(mp.Key, mp.UploadID, mps)
+	// 	rdata, _ := b.Get("testmukey", "0-")
+	// 	readPrint(rdata)
+	// } else {
+	// 	fmt.Println(err)
+	// }
+
+	get := func(key string, off, limit int64) (io.ReadCloser, error) {
+		if off > 0 || limit > 0 {
+			var r string
+			if limit > 0 {
+				r = fmt.Sprintf("%d-%d", off, off+limit-1)
+			} else {
+				r = fmt.Sprintf("%d-", off)
 			}
-			mps = append(mps, p)
+			fmt.Println(key, r)
+			return b.Get(key, r)
 		}
-		lps, err := b.ListParts(mp.Key, mp.UploadID)
-		if err != nil {
-			fmt.Println(err)
+		fmt.Println(key, "")
+		return b.Get(key, "")
+	}
+	//fmt.Println(get)
+	for {
+		size := 5 << 20
+		for i := 0; i < 10; i++ {
+			r, err := get("dir1/CentOS-8.3.2011-aarch64-dvd1.iso", int64(i*size), int64(size))
+			if err != nil {
+				fmt.Println("eee", err)
+			}
+			data := make([]byte, size)
+			_, err = io.ReadFull(r, data)
+			if err != nil {
+				fmt.Println("sss", err)
+			}
+			fmt.Println("bbb", i, len(data))
+			// bts, err = ioutil.ReadAll(r)
+			// if err != nil {
+			// 	fmt.Println(err)
+			// 	return
+			// }
+			// fmt.Println(len(bts))
 		}
-		fmt.Println(mps)
-		fmt.Println(lps)
-		b.CompleteMultipartUpload(mp.Key, mp.UploadID, mps)
-		rdata, _ := b.Get("testmukey", "0-")
-		readPrint(rdata)
-	} else {
-		fmt.Println(err)
 	}
 }
 
